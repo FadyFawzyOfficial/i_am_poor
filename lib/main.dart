9fe274ccbf8dee1cd243fc3930e5629afc909515
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-03 01:23:23 +0200

    1. Work with assets (images) in Flutter and the pubspec.yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp());
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          title: const Text('I am Poor'),
+          backgroundColor: Colors.black87,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/images/coal.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
